3fb8cc8f09d48b50b0570e8c4163f4baaeeec937
FranFigueroa <francofigueroa321@gmail.com>
2023-04-13 13:11:34 -0300

    challenge day 1

---
+++ challenge1.C
@@ -0,0 +1,31 @@
+/*
+The following program will first fill in (by asking the user for input) and then list all the elements in an array
+#include <_____>
+      
+      using namespace std;
+      ___ main()
+{
+  int array[8]___
+  for(int x=_; x<_; ___)
+  cin>>______;
+  for(____; x<____; ___)
+  cout<<_____;
+  return __;
+____*/
+
+//Solution
+
+#Include <stdio.h>
+
+using namespace std;
+int main()
+{
+    int array[8]
+    for(int x=0;x<8;x++)
+    cin>>array[x];
+    for(int x=0; x<8; x++)
+    cout<<array[x];
+    return array[x];
+return 0;
+
+}
